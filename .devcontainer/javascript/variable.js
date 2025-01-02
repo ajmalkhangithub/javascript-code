34f817ba6dfe81c4a6a6660dd8fc7d1410c3eecf
Ajmal khan <144774577+ajmalkhangithub@users.noreply.github.com>
2025-01-02 11:45:04 +0000

    it is a variable

---
+++ .devcontainer/javascript/variable.js
@@ -0,0 +1,13 @@
+const accountId=123;
+let name="Ajmal Khan /n"
+var birth="Sunday"
+// console.log(accountId+"/n "+name+" "+bith );
+console.table([accountId,name,birth]);
+
+
+//operation
+
+console.log(2+2);
+console.log(20-14);
+console.log(2*2);
+console.log(2/2);
