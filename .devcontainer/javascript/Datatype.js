223e423aaf0f670ba4abf788887adc2dd432e2d1
Ajmal khan <144774577+ajmalkhangithub@users.noreply.github.com>
2025-01-02 13:00:30 +0000

    datatype

---
+++ .devcontainer/javascript/Datatype.js
@@ -0,0 +1,26 @@
+//primitive
+
+// 7 String Number ,Bolean ,Undefined ,null, Symbol
+
+//Non-primitive
+// object,Array,function
+
+
+const score=233;
+let isAlpha=true;
+let b=null;
+
+//now non-primitive
+//array
+const arr=["ajmal","khan",4];
+//object
+const student={
+    name:"Ajmal",
+    age:21,
+    dept:"BSIT QAU"
+}
+//Function
+
+function myfunction(){
+    console.log("Hello world")
+}
